926330675d2042cc9ae7ac7081e883e2bfa7547b
Nik Polovenko <nik.polovenko@sezzle.com>
2023-04-19 20:05:49 -0500

    more changes

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   return res.json({ "hello": "world" })
 }
